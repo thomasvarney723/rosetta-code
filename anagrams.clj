adc775886b7ee6df7e412c05d7ca478a4678f187
Thomas Varney <thomasvarney723@gmail.com>
2017-07-10 09:38:37 -0500

    inital commit

---
+++ anagrams.clj
@@ -0,0 +1,21 @@
+;; rosettacode.org "Anagrams"
+
+;; Using the word list at http://www.puzzlers.org/pub/wordlists/unixdict.txt, 
+;; find the sets of words that share the same characters that contain the most 
+;; words in them.
+
+(->> (slurp "http://www.puzzlers.org/pub/wordlists/unixdict.txt")
+     (apply str)
+     clojure.string/split-lines
+     (group-by sort)
+     vals
+     (sort-by count #(compare %2 %1))  ;; sort in reverse
+     (partition-by count)
+     first)
+
+;; (["caret" "carte" "cater" "crate" "trace"] 
+;;  ["angel" "angle" "galen" "glean" "lange"] 
+;;  ["elan" "lane" "lean" "lena" "neal"] 
+;;  ["alger" "glare" "lager" "large" "regal"] 
+;;  ["evil" "levi" "live" "veil" "vile"] 
+;;  ["abel" "able" "bale" "bela" "elba"])
